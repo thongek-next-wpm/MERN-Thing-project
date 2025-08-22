4f048279c6cacfdf270d6585f33d384daba2bdad
thongek-next-wpm <thongek_wpm@hotmail.com>
2025-08-22 15:21:17 +0700

    create front-end

---
+++ frontend/tailwind.config.js
@@ -0,0 +1,13 @@
+import daisyui from "daisyui";
+/** @type {import('tailwindcss').Config} */
+
+export default {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {},
+  },
+  plugins: [daisyui],
+  daisyui: {
+    themes: ["light", "dark", "forest"],
+  },
+};
