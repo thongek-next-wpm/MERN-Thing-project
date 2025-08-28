b53aaf666fa7a5dfafe03a3e9a765a53aad21083
thongek-next-wpm <thongek_wpm@hotmail.com>
2025-08-28 12:33:26 +0700

    add Delete Functionlity

--- frontend/src/components/NoteCard.jsx
+++ frontend/src/components/NoteCard.jsx
@@ -1,8 +1,25 @@
 import { PenSquareIcon, Trash2Icon } from "lucide-react";
 import { Link } from "react-router";
 import { formatDate } from "../lib/utils";
+import toast from "react-hot-toast";
+import axios from "axios";
 
-const NoteCard = ({ note }) => {
+const NoteCard = ({ note, setNotes }) => {
+  const handleDelete = async (e, id) => {
+    e.preventDefault(); // get rid of the navigation behaviour
+
+    if (!window.confirm("Are you sure you want to delete this note?")) return;
+
+    try {
+      await axios.get("http://localhost:5001/api/notes");
+      setNotes((prev) => prev.filter((note) => note._id !== id)); // get rid of the deleted one
+      toast.success("Note deleted successfully");
+    } catch (error) {
+      console.log("Error in handleDelete", error);
+      toast.error("Failed to delete note");
+    }
+  };
+
   return (
     <Link
       to={`/note/${note._id}`}
@@ -18,7 +35,10 @@ const NoteCard = ({ note }) => {
           </span>
           <div className="flex items-center gap-1">
             <PenSquareIcon className="size-4" />
-            <button className="btn btn-ghost btn-xs text-error">
+            <button
+              className="btn btn-ghost btn-xs text-error"
+              onClick={(e) => handleDelete(e, note._id)}
+            >
               <Trash2Icon className="size-4" />
             </button>
           </div>
@@ -27,5 +47,4 @@ const NoteCard = ({ note }) => {
     </Link>
   );
 };
-
 export default NoteCard;
--- frontend/src/views/HomePage.jsx
+++ frontend/src/views/HomePage.jsx
@@ -1,9 +1,10 @@
 import Navbar from "../components/Navbar";
 import { useEffect, useState } from "react";
-import RateLimitdUi from "../components/RateLimitdUi";
+
 import axios from "axios";
 import { toast } from "react-hot-toast";
 import NoteCard from "../components/NoteCard";
+import NotesNotFound from "../components/NotesNotFound";
 
 const HomePage = () => {
   const [isRateLimitd, setIsRateLimitd] = useState(false);
@@ -34,18 +35,20 @@ const HomePage = () => {
   return (
     <div className="min-h-screen">
       <Navbar />
-      {isRateLimitd && <RateLimitdUi />}
+
+      {isRateLimitd && <isRateLimitd />}
 
       <div className="max-w-7xl mx-auto p-4 mt-6">
         {loading && (
-          <div className="text-center text-primary py-10">Loading note....</div>
+          <div className="text-center text-primary py-10">Loading notes...</div>
         )}
+
+        {notes.length === 0 && !isRateLimitd && <NotesNotFound />}
+
         {notes.length > 0 && !isRateLimitd && (
           <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
             {notes.map((note) => (
-              <div>
-                <NoteCard key={note._id} note={note} />
-              </div>
+              <NoteCard key={note._id} note={note} setNotes={setNote} />
             ))}
           </div>
         )}
